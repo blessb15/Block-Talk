63478ff8e6a82d77366759006d045d5daaa4029d
blessb15 <blakebless7@gmail.com>
2016-07-01 08:36:21 -0700

    contain all inital content including a sign up and sign in page.

---
+++ app/src/main/java/com/example/blake/blocktalk/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.blake.blocktalk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.signUp) Button mSignUp;
+    @Bind(R.id.signIn) Button mSignIn;
+
+    private String[] users = {};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+                }
+           });
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
