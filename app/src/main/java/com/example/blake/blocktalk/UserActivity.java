514b99d68250f769a497a5615b7ddd0e93f8b5e8
blessb15 <blakebless7@gmail.com>
2016-07-05 16:21:10 -0700

    contain google map, map is blank at the moment.

--- app/src/main/java/com/example/blake/blocktalk/UserActivity.java
+++ app/src/main/java/com/example/blake/blocktalk/UserActivity.java
@@ -1,26 +1,37 @@
 package com.example.blake.blocktalk;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends FragmentActivity implements OnMapReadyCallback {
     @Bind(R.id.GetUser) TextView mGetUser;
     @Bind(R.id.MessagesView) ListView mMessagesView;
-    private String[] messages = {"Bill: Sup erbody!?!", "Jim: nuttin much, homie g. hbu?", "Michaela: i like chicken enchilada's",
-    "Blake: HAH. me too!!!!!", "Jim: whoever made this app is probably super rich, and owns like 50 cows...", "Blake: yeah, basically."};
+    private String[] messages = {"Bill: hey", "Jim: sup", "Michaela: hi",
+            "Blake: yo", "Jim: sup", "Blake: yo"};
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         ButterKnife.bind(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
 
@@ -28,8 +39,15 @@ public class UserActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
         mMessagesView.setAdapter(adapter);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
