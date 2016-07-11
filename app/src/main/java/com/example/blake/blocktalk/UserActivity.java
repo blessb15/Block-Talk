06e5aaa4e9b064cedea71dbd829c36489e531f3f
blessb15 <blakebless7@gmail.com>
2016-07-11 10:46:14 -0700

    be working as far as messaging.

--- app/src/main/java/com/example/blake/blocktalk/UserActivity.java
+++ app/src/main/java/com/example/blake/blocktalk/UserActivity.java
@@ -8,7 +8,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import java.text.*;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.view.View;
@@ -20,14 +19,11 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.fitness.data.Value;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -42,36 +38,26 @@ public class UserActivity extends FragmentActivity implements OnMapReadyCallback
     @Bind(R.id.SubmitLocalMessage)
     Button mSubmitLocalMessage;
     @Bind(R.id.CurrentLatLongView) TextView mCurrentLatLongView;
-    ArrayList<String> newMessages = new ArrayList<String>();
-    Map <LatLng, ArrayList<String>> mLocationMessages = new HashMap<LatLng, ArrayList<String>>();
+    private ArrayList<String> newMessages = new ArrayList<String>();
+    private Map <LatLng, ArrayList<String>> mLocationMessages = new HashMap<LatLng, ArrayList<String>>();
     private GoogleMap mMap;
-    LocationManager locationManager;
-    Double userLong;
-    Double userLat;
-    LatLng userLocation;
-    //Message visibility radius.
-    Double radius = 0.0003;
-    //lat and long format
-    DecimalFormat df = new DecimalFormat("##.####");
+    private LocationManager locationManager;
+    private Double userLong;
+    private Double userLat;
+    private LatLng userLocation;
+    private Double radius = 0.0003;
 
-    private void getMessages() {
-        for (Map.Entry<LatLng, ArrayList<String>> entry : mLocationMessages.entrySet()) {
-            if (((entry.getKey().latitude + radius) > userLocation.latitude && userLocation.latitude > (entry.getKey().latitude - radius)) && ((entry.getKey().longitude + radius) > userLocation.longitude && userLocation.longitude > (entry.getKey().longitude - radius))) {
-                ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, entry.getValue());
-                System.out.println("this is messages in your location " + entry.getValue());
-                mMessagesView.setAdapter(adapter);
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         ButterKnife.bind(this);
-        //this will make sure you are only seeing messages in your location.
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, newMessages);
+        mMessagesView.setAdapter(adapter);
 
+        //Map fragment setup
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -103,47 +89,43 @@ public class UserActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         ///LOCAL MESSAGE SUBMIT                                                 Location check is wrong, check that you radius and rounding is doing what its supposed to.
-        mSubmitLocalMessage.setOnClickListener(new View.OnClickListener()
-        {
+        mSubmitLocalMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-            //if there are hashes
-            if (mLocationMessages.size() >= 1) {
-                for (Map.Entry<LatLng, ArrayList<String>> entry : mLocationMessages.entrySet()) {
-                    //checks if there is already hash within radius location, if so it just adds a message to it.
-                    if (((entry.getKey().latitude + radius) > userLocation.latitude && userLocation.latitude > (entry.getKey().latitude - radius)) && ((entry.getKey().longitude + radius) > userLocation.longitude && userLocation.longitude > (entry.getKey().longitude - radius))) {
-                        String newMessage = username + ": " + mUserMessage.getText().toString();
-                        entry.getValue().add(newMessage);
-                        mUserMessage.setText("");
-                        mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
-                        //if users location is not within radius of a hash in hashmap, it creates new hash with message.
-                    } else {
-                        String newMessage = username + ": " + mUserMessage.getText().toString();
-                        newMessages.add(newMessage);
-                        mLocationMessages.put(userLocation, newMessages);
-                        mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
-                        mUserMessage.setText("");
+                //if there are hashes
+                if (mLocationMessages.size() >= 1) {
+                    for (Map.Entry<LatLng, ArrayList<String>> entry : mLocationMessages.entrySet()) {
+                        //checks if there is already hash within radius location, if so it just adds a message to it.
+                        if (((entry.getKey().latitude + radius) > userLocation.latitude && userLocation.latitude > (entry.getKey().latitude - radius)) && ((entry.getKey().longitude + radius) > userLocation.longitude && userLocation.longitude > (entry.getKey().longitude - radius))) {
+                            String newMessage = username + ": " + mUserMessage.getText().toString();
+                            entry.getValue().add(newMessage);
+                            mUserMessage.setText("");
+                            mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
+                            //if users location is not within radius of a hash in hashmap, it creates new hash with message.
+                        } else {
+                            String newMessage = username + ": " + mUserMessage.getText().toString();
+                            newMessages.add(newMessage);
+                            mLocationMessages.put(userLocation, newMessages);
+                            mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
+                            mUserMessage.setText("");
+                        }
                     }
                 }
-            }
 
-            //if there are no hashes
-            if (mLocationMessages.size() == 0) {
-                String userMessage = username + ": " + mUserMessage.getText().toString();
-                newMessages.add(userMessage);
-                mLocationMessages.put(userLocation, newMessages);
-                mUserMessage.setText("");
-                mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
-            }
-                if (mLocationMessages.size() >= 0) {
-                    getMessages();
+                //if there are no hashes
+                if (mLocationMessages.size() == 0) {
+                    String userMessage = username + ": " + mUserMessage.getText().toString();
+                    newMessages.add(userMessage);
+                    mLocationMessages.put(userLocation, newMessages);
+                    mUserMessage.setText("");
+                    mCurrentLatLongView.setText("These are the messages at your current " + userLocation);
                 }
             }
-
-
         });
     }
+
+
 
     private final LocationListener listener = new LocationListener() {
         public void onLocationChanged(Location location) {
